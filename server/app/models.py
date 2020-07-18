8155c453b9a5e40e54b6df24f375d3f960c87b47
AliRn76 <alirn76@yahoo.com>
2020-07-18 22:35:49 +0430

    Create Models + Create Token generator + Config the db + Add the api/

---
+++ server/app/models.py
@@ -0,0 +1,28 @@
+from rest_framework.authtoken.models import Token
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+from django.db import models
+
+
+class Item(models.Model):
+    id      = models.AutoField(db_column='ID', primary_key=True)
+    name    = models.CharField(db_column='Name', max_length=96, blank=True, null=True)
+
+    class Meta:
+        db_table = 'Item'
+
+
+class Log(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)
+    text = models.TextField(db_column='Text', blank=True, null=True)
+
+    class Meta:
+        db_table = 'Log'
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+b($soo3l#g1nujr!yhpaf(&@ksaf=5pp7(u7tttfgxzr^r32$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'rest_framework.authtoken',
+    'rest_framework',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +80,11 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'HiCoffee',
+        'USER': '...',
+        'PASSWORD': '...',
+    },
 }
 
 
