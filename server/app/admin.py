4a85a835eef3b76de4f0badd0a215169f7a3a69b
AliRn76 <alirn76@yahoo.com>
2020-08-10 04:18:15 +0430

    I had a mistake so ...

--- server/app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from app.models import Item, Log
-
-admin.site.register(Item)
-admin.site.register(Log)
--- server/app/urls.py
+++
@@ -1,6 +0,0 @@
-
-from django.urls import path, include
-
-urlpatterns = [
-    path('api/', include("app.api.urls")),
-]
